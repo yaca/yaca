89bc0ba43de84cbed9104eea1b438c5773e33d72
Suca <sushanthkr@gmail.com>
2011-02-23 22:54:01 -0800

    Created YACA!!

---
+++ src/com/yaca/Yaca.java
@@ -0,0 +1,13 @@
+package com.yaca;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Yaca extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
